f0be14ca04cf666fcba6c8b1a6dcb253f9c81d3e
BlackkTurtle <113180669+BlackkTurtle@users.noreply.github.com>
2025-03-15 12:23:52 +0200

    Добавьте файлы проекта.

---
+++ SampleCabDataETLProject.App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCabDataETLProject.App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
